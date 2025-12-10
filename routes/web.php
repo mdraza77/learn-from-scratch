b9935bce8d06396143092a96b7b72283fb2855d5
Md Raza <mdraza8397@gmail.com>
2025-12-10 20:22:20 +0530

    10122025 001 routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home/{name}', function ($name) {
+    echo $name;
+    return view('home', ['name' => $name]);
+});
+
+// Route::redirect('/home', '/');
+
+Route::view('/about', 'welcome');
