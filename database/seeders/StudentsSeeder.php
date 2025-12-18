1e52143fbe71629f33d190a696844f849dc7cf0d
Md Raza <mdraza8397@gmail.com>
2025-12-18 21:03:44 +0530

    18122025 001 Seeders

---
+++ database/seeders/StudentsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+class StudentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('students')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10) . '@gmail.com',
+            'batch' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
