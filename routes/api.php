a718325639a1591a7de49cbb864a761835e6937d
Md Raza <mdraza8397@gmail.com>
2025-12-21 22:17:01 +0530

    21122025 004 api installed and tests using 1route and controller

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\StudentControlle;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function() {
+    return ['name' => "Raza"];
+});
+
+Route::get('/students', [StudentControlle::class, 'index']);
